9575837484c544f962575c81e789d9b1fa8bca07
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-04-15 14:38:41 -0300

    Salvando curso

---
+++ src/application/_17_IntroducaoProgramacaoOrientadaAObjeto.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class _17_IntroducaoProgramacaoOrientadaAObjeto {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double xA, xB, xC, yA, yB, yC;
+		System.out.println("Determine a medida do triangulo X: ");
+		xA = sc.nextDouble();
+		xB = sc.nextDouble();
+		xC = sc.nextDouble();
+		System.out.println("Determine a medida do triangulo Y: ");
+		yA = sc.nextDouble();
+		yB = sc.nextDouble();
+		yC = sc.nextDouble();
+		
+		//Fórmula para calcular a área do triangulo X... fornecido no material de estudos.
+		double p = (xA + xB + xC) / 2.0;
+		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		p = (yA + yB + yC) / 2.0;
+		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		
+		System.out.printf("Area do triangulo X: %.4f%n", areaX);
+		System.out.printf("Area do triangulo Y: %.4f%n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("A maior área é a do: 'X'");
+		}else{
+			System.out.println("A maior área é a do: 'Y'");
+		}
+		System.out.println("Programa terminou!");
+		System.out.println("Até a próxima =)");
+		sc.close();
+	}
+}
