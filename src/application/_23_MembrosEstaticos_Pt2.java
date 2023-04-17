26d4d310a7b7aa64d857ea12080c84d146db74f3
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-04-17 15:12:15 -0300

    Salvando curso

---
+++ src/application/_23_MembrosEstaticos_Pt2.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class _23_MembrosEstaticos_Pt2{
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Programa terminou!");
+		System.out.println("Até a próxima =)");
+		sc.close();
+	}
+}
