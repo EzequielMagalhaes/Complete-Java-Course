765c9c746488d8d0cab9e5846c74db5ce7c698ce
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-04-17 15:09:58 -0300

    Salvando curso

---
+++ src/exercises/Exercicios_17_21.java
@@ -0,0 +1,27 @@
+package exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicios_17_21 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Programa terminou!");
+		System.out.println("Até a próxima =)");
+		sc.close();
+	}
+}
+/*EXERCÍCIOS
+1. Fazer um programa para ler os valores da largura e altura de um retângulo. Em seguida mostrar na tela o valor de sua área, perímetro e diagonal.
+Usar uma classe como mostrado no projeto. 
+*
+2. Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). Em seguida, mostrar os dados do funcionário com base em
+uma porcentagem dade (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário.
+*
+3. Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano (primeiro trimestre vale 30 e o segundo
+e terceiro valem 35 cada). Ao final, mostrar o qual a nota final do aluno no ano. Dizer também se o aluno está aprovado (PASS) ou não (FAILED) e, 
+em caso negativo, quantos pontos faltam para o aluno obter o mínimo para ser aprovado (que é 60% da nota). Deve criar uma classe 'Estudante' 
+para resolver os problemas.
+*/
--- src/exercises/Exercicios_6.java
+++ src/exercises/Exercicios_6.java
@@ -2,7 +2,7 @@ package exercises;
 
 import java.util.Locale;
 import java.util.Scanner;
-public class Exercícios_6 {
+public class Exercicios_6 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
