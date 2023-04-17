c95936765f98a679b7cde75a15975b2823775345
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-04-17 15:01:16 -0300

    Salvando curso

--- src/application/_21_Object_toString.java
+++ src/application/_21_Object_toString.java
@@ -4,20 +4,32 @@ import java.util.Locale;
 import java.util.Scanner;
 import entities.Produto;
 
-public class _21_ObjectToString {
+public class _21_Object_toString {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		Produto produto = new Produto();
 		System.out.println("Insira os dados do produto: ");
-		System.out.print("Name: ");
+		System.out.print("Nome: ");
 		produto.nome = sc.nextLine();
 		System.out.print("Preço: ");
 		produto.preco = sc.nextDouble();
 		System.out.print("Quantidade em estoque: ");
 		produto.quantidade = sc.nextInt();
-		System.out.println(produto); //nem precisa puxar o método 'toString()' , o eclipse já reconhece.
+		System.out.println(); // string vazia, só para saltar linha.
+		System.out.println("Dados do produto: " + produto); //nem precisa puxar o método 'toString()' , o eclipse já reconhece.
+		System.out.println();
+		System.out.print("Insira o número de produtos para serem adicionados ao estoque: ");
+		int quantidade = sc.nextInt();
+		produto.addProdutos(quantidade);
+		System.out.println();
+		System.out.println("Dados atualizados: " + produto);
+		System.out.print("Insira o número de produtos para serem removidos do estoque: ");
+		quantidade = sc.nextInt();
+		produto.removerProdutos(quantidade);
+		System.out.println();
+		System.out.println("Dados atualizados: " + produto);
 		System.out.println("Programa terminou!");
 		System.out.println("Até a próxima =)");
 		sc.close();
