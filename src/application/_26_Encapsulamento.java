887d7e6352aa46170ed008d7b233be8ae84e8e08
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-04-27 11:08:38 -0300

    Salvando curso

--- src/application/_26_Encapsulamento.java
+++ src/application/_26_Encapsulamento.java
@@ -2,11 +2,39 @@ package application;
 
 import java.util.Locale;
 import java.util.Scanner;
+import entities.Produto3;
 
 public class _26_Encapsulamento {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Insira os dados do produto: ");
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Preço: ");
+		double preco = sc.nextDouble();
+		Produto3 produto = new Produto3();
+		
+		produto.setNome("Computador");
+		System.out.println("Nome atualizado: " + produto.getNome());
+		produto.setPreco(1200.00);
+		System.out.println("Preço atualizado: " + produto.getPreco());
+		
+		System.out.println();
+		System.out.println("Dados do produto: " + produto);
+		System.out.println();
+		System.out.print("Insira o número de produtos para serem adicionados ao estoque: ");
+		int quantidade = sc.nextInt();
+		produto.addProdutos(quantidade);
+		System.out.println();
+		System.out.println("Dados atualizados: " + produto);
+		System.out.print("Insira o número de produtos para serem removidos do estoque: ");
+		quantidade = sc.nextInt();
+		produto.removerProdutos(quantidade);
+		System.out.println();
+		System.out.println("Dados atualizados: " + produto);
+		System.out.println();
 		
 		System.out.println("Programa terminou!");
 		System.out.println("Até a próxima =)");
---
+++ src/entities/Produto3.java
@@ -0,0 +1,57 @@
+//	NOVA CLASSE PARA DEMONSTRAR OS GETTERS E SETTERS //
+package entities;
+
+public class Produto3{
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public Produto3(){
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double totalEmEstoque(){
+		return preco * quantidade;
+	}
+	
+	public void addProdutos(int quantidade){
+		this.quantidade += quantidade;
+	}
+	
+	public void removerProdutos(int quantidade){
+		this.quantidade -= quantidade;
+	}
+	
+	public String toString(){
+		return nome
+			+ ", R$: "
+			+ String.format("%.2f",preco)
+			+ ", "
+			+ quantidade
+			+ " unidades, Total: R$: "
+			+ String.format("%.2f",totalEmEstoque());
+	}
+}
