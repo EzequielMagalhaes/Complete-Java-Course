724c265dd59195a3882ebb1046c0400ab3beba01
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-05-08 15:25:56 -0300

    Salvando curso

---
+++ src/application/_34_Matrizes.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class _34_Matrizes {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Programa terminou!");
+		System.out.println("Até a próxima =)");
+		sc.close();
+	}
+}
+/*				MATRIZ
+*	° Em programação, "Matriz" é o nome dado a arrays bidimensionais.
+*		."Vetores de vetores" 
+* 	° Array (arranjo) é uma estrutura de dados:
+* 		- Homogênea (dados do mesmo tipo).
+* 		- Ordenada (elemnetos acessados por meio de posições).
+* 		- Alocada de uma vez só, em um bloco contíguo de memória.
+* 	° Vantagens:
+* 		- Acesso imediato aos elementos pela sua posição.
+* 	° Desvantagens:
+* 		- Tamanho fixo.
+* 		- Dificuldade para se realizar inserções e deleções.
+*/
