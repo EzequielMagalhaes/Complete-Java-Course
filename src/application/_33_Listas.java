72518ab58be1f48155a3d65e7cf04d6d2c2427b6
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-05-08 15:03:40 -0300

    Salvando curso

--- src/application/_33_Listas.java
+++ src/application/_33_Listas.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class _33_Listas {
 	public static void main(String[] args) {
@@ -16,17 +17,48 @@ public class _33_Listas {
 		lista.add("Nathália"); // [1]
 		lista.add("Samuel"); // [2]
 		lista.add("Lyssa"); // [4]
+		lista.add("Maria"); // [5]		
+		lista.add("Leandro"); // [6]		
 		lista.add(3,"Gaby"); // [3]
-		
-		lista.remove("Samuel"); // esse "Samuel" é outra instância de string... uma lista é capaz de fazer uma comparação entre strings e pelo valor reconhecer e remover a string da ln.17
-		lista.remove(3); // da pra remover através do index de posição.
 		
 		for(String x : lista){
 			System.out.println(x);
 		}
-		
 		System.out.println("O tamanho da lista é : " + lista.size() + " elemento(s)");
+		System.out.println("--------------------");
+		for(String y : lista){
+			System.out.println("Index do(a) " + y + ": " + lista.indexOf(y));
+		}
+		System.out.println("--------------------");
+//		O tipo 'stream()' é um tipo especial do Java 8+ que aceita operações com expressões lambda
+		List<String> resultado = lista.stream().filter(x -> x.charAt(0) == 'L').collect(Collectors.toList()); 
+		for(String z : resultado){
+			System.out.println(z);
+		}
+		System.out.println("São os nomes que começam com : L" );
+		System.out.println("--------------------");
+//		REMOVER POR COMPARAÇÃO DE VALORES
+		lista.remove("Samuel"); // esse "Samuel" é outra instância de string... uma lista é capaz de fazer uma comparação entre strings e pelo valor reconhecer e remover a string da ln.17
+//		REMOVER POR INDEX DE POSIÇÃO
+		lista.remove(3);
+//		REMOVER POR PREDICADO
+		lista.removeIf(x -> x.charAt(0) == 'M'); //função lambda, remove todos os nomes começados com "M".
 		
+		for(String x : lista){
+			System.out.println(x);
+		}
+		System.out.println("O tamanho da nova lista é : " + lista.size() + " elemento(s)");
+		System.out.println("--------------------");
+		
+		for(String y : lista){
+			System.out.println("Index do(a) " + y + ": " + lista.indexOf(y));
+		}
+		System.out.println("--------------------");
+
+//		Pega o primeiro elemento que atende ao predicado(x -> x.charAt(0) == 'N'), se esse elemento não existir ele vai retornar null;
+		String nome = lista.stream().filter(x -> x.charAt(0) == 'N').findFirst().orElse(null);
+		System.out.println(nome);
+		System.out.println("--------------------");
 		System.out.println("Programa terminou!");
 		System.out.println("Até a próxima =)");
 		sc.close();
