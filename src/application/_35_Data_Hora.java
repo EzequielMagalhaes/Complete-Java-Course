a1c8f9dfa782a8712d1668ca2d66829433c8a65c
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-05-11 15:10:17 -0300

    Salvando curso

--- src/application/_35_Data_Hora.java
+++ src/application/_35_Data_Hora.java
@@ -1,12 +1,48 @@
 package application;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class _35_Data_Hora {
+//	JAVADOC DO DATETIME FORMATTER
+//	https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatterBuilder.html
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter formato1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter formato2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		LocalDate d01 = LocalDate.now();
+		LocalDateTime d02 = LocalDateTime.now();
+		Instant d03 = Instant.now(); // vai gerar a data-hora no horário de Londres.
+		
+		LocalDate d04 = LocalDate.parse("2023-05-11"); // '.parse()' server para formatar no formato ISO-8601.
+		LocalDateTime d05 = LocalDateTime.parse("2023-05-11T14:35:40");
+		Instant d06 = Instant.parse("2023-05-11T14:35:40Z");
+		Instant d07 = Instant.parse("2023-05-11T14:35:40-03:00");
+		
+		LocalDate d08 = LocalDate.parse("11/05/2023",formato1); // instanciando com um formato customizado. 
+		LocalDateTime d09 = LocalDateTime.parse("11/05/2023 14:52", formato2); // o expressão pode ser colocada na mesma linha ao invés da variável 'formato2'.
+		LocalDate d10 = LocalDate.of(2023, 05, 11);
+		LocalDateTime d11 = LocalDateTime.of(2023, 05, 11, 15, 9);
+		
+		
+		System.out.println("d01 = " + d01);
+		System.out.println("d02 = " + d02);
+		System.out.println("d03 = " + d03);
+		System.out.println("d04 = " + d04);
+		System.out.println("d05 = " + d05);
+		System.out.println("d06 = " + d06);
+		System.out.println("d07 = " + d07);
+		System.out.println("d08 = " + d08);
+		System.out.println("d09 = " + d09);
+		System.out.println("d10 = " + d10);
+		System.out.println("d11 = " + d11);
 		
 		System.out.println("Programa terminou!");
 		System.out.println("Até a próxima =)");
