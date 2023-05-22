0f231b984cb600805cf32dafaf9a1d9fc73fbd4b
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-05-22 16:05:13 -0300

    Salvando curso

--- src/application/_40_Calendar.java
+++ src/application/_40_Calendar.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,6 +10,10 @@ public class _40_Calendar {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2023-05-22T15:59:40Z"));
+		System.out.println(sdf.format(d));
 		
 		System.out.println("Programa terminou!");
 		System.out.println("Até a próxima =)");
