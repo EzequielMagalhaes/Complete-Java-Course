e2698bf600f1900860975af46217edacb4b98bed
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-05-22 17:20:57 -0300

    Salvando curso

--- src/application/_41_Enumeracoes.java
+++ src/application/_41_Enumeracoes.java
@@ -1,15 +1,27 @@
 package application;
 
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
+import entities.Pedido;
+import entities.enums.StatusPedido;
 
 public class _41_Enumeracoes {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		Pedido pedido = new Pedido(1080, new Date(), StatusPedido.AGUARDANDO_PAGAMENTO);
+		System.out.println(pedido);
+		
 		System.out.println("Programa terminou!");
 		System.out.println("Até a próxima =)");
 		sc.close();
 	}
 }
+/*				ENUMERAÇÃO
+	° É um tipo especial que serve para especificar de forma literal um conjunto de constantes relacionadas.
+	° Palavra chave em Java: 'enum'.
+	° Vantagem: melhor semântica, código mais legível e auxiliado pelo compilador
+	° JAVA DOC -> https://docs.oracle.com/javase/8/docs/api/java/lang/Enum.html
+*/
---
+++ src/entities/Pedido.java
@@ -0,0 +1,48 @@
+package entities;
+
+import java.util.Date;
+import entities.enums.StatusPedido;
+
+public class Pedido {
+	private Integer id;
+	private Date instante;
+	private StatusPedido status;
+
+	public Pedido() {
+	}
+	
+	public Pedido(Integer id, Date instante, StatusPedido status) {
+		this.id = id;
+		this.instante = instante;
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id= " + id + ", instante= " + instante + ", status= " + status + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getinstante() {
+		return instante;
+	}
+
+	public void setinstante(Date instante) {
+		this.instante = instante;
+	}
+
+	public StatusPedido getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
+}
