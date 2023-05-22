5219ee1532930989bdee04974f50a72c78a8e7d7
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-05-22 15:55:14 -0300

    Salvando curso

--- src/application/_41_Enumeracoes.java
+++ src/application/_41_Enumeracoes.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class _40_Enumeracoes {
+public class _41_Enumeracoes {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
