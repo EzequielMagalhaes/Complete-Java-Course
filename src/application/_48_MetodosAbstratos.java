baa8b0920fdb8400b89910238251a0bfaab367cc
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-06-14 17:58:09 -0300

    Salvando curso

--- src/application/_48_MetodosAbstratos.java
+++ src/application/_48_MetodosAbstratos.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class _48_ {
+public class _48_MetodosAbstratos {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
@@ -14,3 +14,8 @@ public class _48_ {
 		sc.close();
 	}
 }
+/*			MÉTODOS ABSTRATOS
+	° São métodos que não possuem implementação.
+	° Métodos precisam ser abstratos quando a classe é genérica demais para conter sua implementação.
+	° Se uma classe possuir pelo menos um método abstrato, então esta classe também é abstrata.
+*/
