a6a02f9f43d4723ec354a17125473b977285029e
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-06-17 12:28:43 -0300

    Salvando curso

---
+++ src/exercises/Exercicio_52_54.java
@@ -0,0 +1,20 @@
+package exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio_52_54 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("===================================================================");
+		System.out.println("Programa terminou!");
+		System.out.println("Até a próxima =)");
+		sc.close();
+	}
+}
+/*EXERCÍCIO -TEM CORREÇÃO!
+1. Fazer um programa para ler os dados de uma conta bancária e depois realizar um saque nesta conta bancária, mostrando o novo saldo. Um saque não
+pode ocorrer ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de saque da conta.
+*/
