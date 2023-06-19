95d5d71da5dfd93b4417c3a6ffdfa1deaf54208e
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-06-19 16:15:36 -0300

    Salvando curso

--- src/application/_55_File_Scanner.java
+++ src/application/_55_File_Scanner.java
@@ -1,17 +1,34 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class _55_File_Scanner {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		
+		File file = new File("c:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch(IOException e){
+			System.out.println("Error" + e.getMessage());
+		}
+		finally {
+			if(sc != null) {				
+				sc.close();
+			}
+		}
 		
 		System.out.println("===================================================================");
 		System.out.println("Programa terminou!");
 		System.out.println("Até a próxima =)");
-		sc.close();
 	}
 }
 /*
