19d455f0c02492026e959550b5a9eb1c657820ea
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-06-29 11:26:44 -0300

    Salvando curso

---
+++ exercises/Exercicios_55_60.java
@@ -0,0 +1,25 @@
+package exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicios_55_60 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		
+		
+		System.out.println("===================================================================");
+		System.out.println("Programa terminou!");
+		System.out.println("Até a próxima =)");
+		sc.close();
+	}
+}
+/*EXERCÍCIO - TEM CORREÇÃO!
+1. Fazer um programa para ler o caminho de um arquivo '.csv' contendo os dados de itens vendidos. Cada item possui
+um nome, preço unitário e quantidade, separados por vírgula. Você deve gerar um novo arquivo chamado 'summary.csv',
+localizado em uma subpasta chamada 'out' a partir da pasta original do arquivo de origem, contendo apenas o nome e o
+valor total para aquele item (preço unitário multiplicado pela quantidade).
+
+*/
